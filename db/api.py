109ea15b6d672dc42795bab5e2556067529cb4f8
Adam Hanna <KrakeAdam@proton.me>
2026-05-29 13:24:50 +1000

    added API basics and compliance

---
+++ db/api.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+
+from db_test import (
+    create_compliant_route,
+    create_route,
+    lookup_id_from_osm_id,
+    structure_route_as_dict,
+)
+
+app = Flask(__name__)
+
+
+@app.route("/route")
+def route():
+    osm_id_1 = request.args.get("osm_id_1")
+    osm_id_2 = request.args.get("osm_id_2")
+
+    id_1 = lookup_id_from_osm_id(osm_id_1)
+    id_2 = lookup_id_from_osm_id(osm_id_2)
+
+    if not osm_id_1 or not osm_id_2:
+        return jsonify({"error": "from and to query params required"}), 400
+
+    route = structure_route_as_dict(create_compliant_route(id_1, id_2))
+    return jsonify(route)
