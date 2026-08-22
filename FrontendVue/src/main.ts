efa6580fd2c7a4dcf3989715e4fd1b2550253052
Adam Hanna <KrakeAdam@proton.me>
2026-08-22 22:16:15 +1000

    Reorganisation of github part 1: Removal of all duplicate or unnecessary changes (Pre-this commit changes are stored in a seperate branch if I miss anything, and also the git history)

--- FrontendVue/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import router from "./router";
-import "leaflet/dist/leaflet.css";
-import "maplibre-gl/dist/maplibre-gl.css";
-createApp(App).use(router).mount("#app");
